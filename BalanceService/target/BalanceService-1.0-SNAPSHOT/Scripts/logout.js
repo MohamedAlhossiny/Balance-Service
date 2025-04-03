3953bf731f20ee9fd1154490b3c89a8c2d7b32fa
MohamedAlhossiny <90796706+MohamedAlhossiny@users.noreply.github.com>
2025-04-04 00:58:07 +0200

    Delete BalanceService/target directory

--- BalanceService/target/BalanceService-1.0-SNAPSHOT/Scripts/logout.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("logoutBtn").addEventListener("click", function () {
-    localStorage.removeItem("loggedInUser");
-    localStorage.removeItem("role");
-    window.location.href = "login.html"; 
-});
